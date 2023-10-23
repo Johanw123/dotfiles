937da1c905ef946b3ba28985aa911ed74079128f
Johanw123 <johanw123@gmail.com>
2023-10-23 18:34:03 +0200

    files

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,37 +1,82 @@
+$ENV:STARSHIP_CONFIG = "$HOME\dotfiles\Powershell\starship.toml"
+
+function Invoke-Starship-PreCommand {
+  $host.ui.Write("`e]0; terminal `a")
+}
+
 Invoke-Expression (&starship init powershell)
 
+
+
+Set-Alias -Name np -Value C:\Windows\notepad.exe
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name vim -Value nvim
 
-Set-Alias grep findstr
-Set-Alias touch New-Item
 
+function cd-susrepo1 { set-location "G:\dev\susrepo1" }
+new-alias cd-sus gotosusrepo1
+function cd-susrepo1_2 { set-location "G:\dev\susrepo1_2" }
+new-alias cd-sus2 gotosusrepo1_2
+
+function cd_appl { set-location C:\Users\Johan\AppData\Local }
+function cd-home { set-location "C:\Users\johanw" }
+function cd-nvim { set-location "C:\Users\johanw\AppData\Local\nvim" }
+function cd-conan { set-location "C:\Users\johanw\.conan" }
+
+function cd-dsus { set-location D:\Dev\SurgicalScience }
+
+function cd_devc { set-location C:\Dev }
+function cd_devd { set-location D:\Dev }
+
+function cd-smb { set-location C:\Dev\SquareManBoy }
+function cd-dr { set-location D:\Dev\DungeonRun\DungeonRun }
+
+
+
+function ls_alias_detailed { Get-ChildItem $args -Exclude .* }
+Set-Alias -Name lsd -Value ls_alias_detailed -Option AllScope
+
+function ls_alias_c1 { Get-ChildItem $args -Exclude .*  | Format-Wide -Column 1 -Property Name }
+Set-Alias -Name ls1 -Value ls_alias_c1 -Option AllScope
+
+function ls_alias_c2 { Get-ChildItem $args -Exclude .*  | Format-Wide -Column 2 -Property Name }
+Set-Alias -Name ls2 -Value ls_alias_c2 -Option AllScope
+
+function ls_alias_c3 { Get-ChildItem $args -Exclude .*  | Format-Wide -Column 3 -Property Name }
+Set-Alias -Name ls3 -Value ls_alias_c3 -Option AllScope
+
+function ls_alias_c4 { Get-ChildItem $args -Exclude .*  | Format-Wide -Column 4 -Property Name }
+Set-Alias -Name ls4 -Value ls_alias_c4 -Option AllScope
+
+function ls_alias_c5 { Get-ChildItem $args -Exclude .*  | Format-Wide -Column 5 -Property Name }
+Set-Alias -Name ls5 -Value ls_alias_c5 -Option AllScope
+
+function ls_alias_unix { wsl ls --color=auto -hF $args }
+Set-Alias -Name lsu -Value ls_alias_unix -Option AllScope
+
+function lse_alias_unix { wsl exa --icons -F -H --group-directories-first --git -1 }
+Set-Alias -Name lse -Value lse_alias_unix -Option AllScope
+
+function ll_alias_unix { wsl exa --icons -F -H --group-directories-first --git -1 -alF }
+Set-Alias -Name ll -Value ll_alias_unix -Option AllScope
+
+function ls_alias { lse }
+Set-Alias -Name ls -Value ls_alias_detailed -Option AllScope
+
+
+function drives {Get-PsDrive -PSProvider 'FileSystem'}
 Set-Alias ds drives
-
-Set-Alias wget Invoke-WebRequest
 
 Set-Alias zip Compress-Archive
 Set-Alias unzip Expand-Archive
 
-Set-Alias g git
+function ssh_truls { ssh johanw@172.31.232.86 }
+new-alias truls ssh_truls
+
+function ssh_linbox { ssh root@172.31.231.5 }
+new-alias linbox ssh_linbox
 
 function which ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
         Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 } 
-
-function drives {gdr -PSProvider 'FileSystem'}
-
-function cd-nvim { cd C:\Users\Johan\AppData\Local\nvim }
-function cd-smb { cd C:\Dev\SquareManBoy }
-function cd-dr { cd D:\Dev\DungeonRun\DungeonRun }
-
-function cd_appl { cd C:\Users\Johan\AppData\Local }
-function cd_app { cd C:\Users\Johan\AppData }
-function cd_appr { cd C:\Users\Johan\AppData\Roaming }
-
-function cd_devc { cd C:\Dev }
-function cd_devd { cd D:\Dev }
-
-function cd-sus { cd D:\Dev\SurgicalScience }
-function cd-susrepo { cd D:\Dev\SurgicalScience\susrepo1}
--- install.ps1
+++ install.ps1
@@ -1,4 +1,19 @@
-# clone my Dotfiles repo
+
+# Prereqs
+
+#choco install ripgrep
+#choco install fzf
+#dotnet tool install --global csharp-ls
+
+#Powershell
+#Install-Module PSUtil # https://github.com/PowershellFrameworkCollective/PSUtil
+#choco install starship
+
+# Dotfiles
+#git clone https://github.com/NvChad/NvChad $HOME\AppData\Local\nvim --depth 1
+#rm $HOME\AppData\Local\nvim\lua\custom # We link our own custom lua here from dotfiles later
+
+# Clone dotfiles repo
 $dotFilesRoot = Join-Path $HOME "dotfiles"
 
 if (!(Test-Path $dotFilesRoot -PathType Container)) {
