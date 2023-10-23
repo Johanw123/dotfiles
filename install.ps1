0a22cbeba53645926e1bfcb030deb929706e7e2d
Johanw123 <johanw123@gmail.com>
2023-10-23 17:58:19 +0200

    files

--- install.ps1
+++ install.ps1
@@ -1,7 +1,31 @@
 # clone my Dotfiles repo
-$dotFilesRoot = Join-Path $HOME "dotfiles2"
+$dotFilesRoot = Join-Path $HOME "dotfiles"
 
 if (!(Test-Path $dotFilesRoot -PathType Container)) {
-    git clone git@github.com:Johanw123/dotfiles.git $dotFilesRoot
-    #echo "awdawd"
+    git clone https://github.com/Johanw123/dotfiles.git $dotFilesRoot
+}
+
+
+# link NeoVim configuration
+$localConfiguration = Join-Path $env:LOCALAPPDATA "nvim" "lua" "custom"
+$dotfilesConfiguration = Join-Path $dotFilesRoot ".config" "nvim" "lua" "custom"
+
+if (!(Test-Path $localConfiguration -PathType Container)) { 
+    Start-Process -FilePath "pwsh" -ArgumentList "-c New-Item -Path $localConfiguration -ItemType SymbolicLink -Value $dotfilesConfiguration".Split(" ") -Verb runas
+}
+
+# link Omnisharp configuration
+$localConfiguration = Join-Path $HOME ".omnisharp"
+$dotfilesConfiguration = Join-Path $dotFilesRoot ".omnisharp"
+
+if (!(Test-Path $localConfiguration -PathType Container)) { 
+    Start-Process -FilePath "pwsh" -ArgumentList "-c New-Item -Path $localConfiguration -ItemType SymbolicLink -Value $dotfilesConfiguration".Split(" ") -Verb runas
+}
+
+# link Powershell configuration
+$localConfiguration = Join-Path $HOME "Documents" "PowerShell" "Microsoft.PowerShell_profile.ps1"
+$dotfilesConfiguration = Join-Path $dotFilesRoot "Powershell" "Microsoft.PowerShell_profile.ps1"
+
+if (!(Test-Path $localConfiguration -PathType Container)) { 
+    Start-Process -FilePath "pwsh" -ArgumentList "-c New-Item -Path $localConfiguration -ItemType SymbolicLink -Value $dotfilesConfiguration".Split(" ") -Verb runas
 }
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+#ln -sf ~/.dot_files/linux/gemrc ~/.gemrc
+#ln -sf ~/.dot_files/linux/gtkrc-2.0.mine ~/.gtkrc-2.0.mine
+#ln -sf ~/.dot_files/linux/i3 ~/.i3
+#ln -sf ~/.dot_files/linux/irbrc ~/.irbrc
+#ln -sf ~/.dot_files/linux/p10k.zsh ~/.p10k.zsh
+#ln -sf ~/.dot_files/linux/screenlayout ~/.screenlayout
+#ln -sf ~/.dot_files/linux/tmux/tmux.conf ~/.tmux.conf
+#ln -sf ~/.dot_files/linux/Xmodmap ~/.Xmodmap
+#ln -sf ~/.dot_files/linux/zinit/zshrc ~/.zshrc
+#ln -sf ~/.dot_files/linux/gh-packer ~/.gh-packer
+#ln -sf ~/.dot_files/extras/cava ~/.config/cava
+#ln -sf ~/.dot_files/linux/compton ~/.config/compton
+#ln -sf ~/.dot_files/linux/kitty ~/.config/kitty
+#ln -sf ~/.dot_files/linux/lazygit ~/.config/jesseduffield/
+#ln -sf ~/.nv-ide ~/.config/nvim
+#ln -sf ~/.dot_files/linux/polybar ~/.config/polybar
+#ln -sf ~/.dot_files/linux/ranger ~/.config/ranger
+#ln -sf ~/.dot_files/linux/rofi ~/.config/rofi
+#ln -sf ~/.dot_files/linux/wezterm.lua ~/.wezterm.lua
+#ln -sf ~/.dot_files/linux/tool-versions ~/.tool-versions
+#ln -sf ~/.dot_files/linux/rvm_default.gems ~/.default-gems
+#ln -sf ~/.dot_files/linux/nvm_default_packages ~/.default-npm-packages
+#ln -sf ~/.dot_files/linux/asdfrc ~/.asdfrc
+#ln -sf ~/.v-ide ~/.vim
+#ln -sf ~/.v-ide/vimrc ~/.vimrc
+#ln -sf ~/.dot_files/linux/nvm_default_packages ~/.nvm/default-packages
+#mkdir ~/.tmux
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
