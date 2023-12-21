d3dcd8a050f56ac6e759e903df0f0d292b606622
Johan Wångsell <Johan.Wangsell@surgicalscience.com>
2023-12-21 15:54:07 +0100

    stuff

--- install_cuda_12.2.sh
+++ install_cuda_12.2.sh
@@ -5,3 +5,7 @@ sudo dpkg -i cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
+export PATH=/usr/local/cuda-12.2/bin${PATH:+:${PATH}}
+
+sudo apt update
+sudo apt install -y cuda-toolkit-12-2
