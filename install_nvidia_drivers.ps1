aee930adf3d07cc7b57b6f6be83826d20314fd88
Johanw123 <johanw123@gmail.com>
2024-03-18 10:44:08 +0100

    wezterm stuff

--- install_nvidia_drivers.ps1
+++ install_nvidia_drivers.ps1
@@ -10,12 +10,6 @@ param (
     [string]$folder = "$env:temp"   # Downloads and extracts the driver here
 )
 
-
-$scheduleTask = $false  # Creates a Scheduled Task to run to check for driver updates
-$scheduleDay = "Sunday" # When should the scheduled task run (Default = Sunday)
-$scheduleTime = "12pm"  # The time the scheduled task should run (Default = 12pm)
-
-
 # Checking if 7zip or WinRAR are installed
 # Check 7zip install path on registry
 $7zipinstalled = $false 
@@ -39,28 +33,17 @@ elseif ($7zipinstalled -eq $false) {
 }
 else {
     Write-Host "Sorry, but it looks like you don't have a supported archiver."
-    Write-Host ""
-    while ($choice -notmatch "[y|n]") {
-        $choice = read-host "Would you like to install 7-Zip now? (Y/N)"
-    }
-    if ($choice -eq "y") {
-        # Download and silently install 7-zip if the user presses y
-        $7zip = "https://www.7-zip.org/a/7z1900-x64.exe"
-        $output = "$PSScriptRoot\7Zip.exe"
-        (New-Object System.Net.WebClient).DownloadFile($7zip, $output)
-       
-        Start-Process "7Zip.exe" -Wait -ArgumentList "/S"
-        # Delete the installer once it completes
-        Remove-Item "$PSScriptRoot\7Zip.exe"
-    }
-    else {
-        Write-Host "Press any key to exit..."
-        $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-        exit
-    }
+    Write-Host "Installing 7-zip..."
+   
+    $7zip = "https://www.7-zip.org/a/7z1900-x64.exe"
+    $output = "$PSScriptRoot\7Zip.exe"
+    (New-Object System.Net.WebClient).DownloadFile($7zip, $output)
+    
+    Start-Process "7Zip.exe" -Wait -ArgumentList "/S"
+    # Delete the installer once it completes
+    Remove-Item "$PSScriptRoot\7Zip.exe"
 }
    
-
 
 # Checking currently installed driver version
 Write-Host "Attempting to detect currently installed driver version..."
@@ -70,8 +53,8 @@ try {
 }
 catch {
     Write-Host -ForegroundColor Yellow "Unable to detect a compatible Nvidia device."
-    Write-Host "Press any key to exit..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    #Write-Host "Press any key to exit..."
+    #$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
 Write-Host "Installed version `t$ins_version"
@@ -100,8 +83,8 @@ Write-Output "Latest version `t`t$version"
 # Comparing installed driver version to latest driver version from Nvidia
 if (!$clean -and ($version -eq $ins_version)) {
     Write-Host "The installed version is the same as the latest version."
-    Write-Host "Press any key to exit..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    #Write-Host "Press any key to exit..."
+    #$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
 
@@ -160,8 +143,8 @@ elseif ($archiverProgram -eq $winrarpath) {
 }
 else {
     Write-Host "Something went wrong. No archive program detected. This should not happen."
-    Write-Host "Press any key to exit..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    #Write-Host "Press any key to exit..."
+    #$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
 
@@ -178,34 +161,20 @@ if ($clean) {
 }
 Start-Process -FilePath "$extractFolder\setup.exe" -ArgumentList $install_args -wait
 
-
-# Creating a scheduled task if the $scheduleTask varible is set to TRUE
-if ($scheduleTask) {
-    Write-Host "Creating A Scheduled Task..."
-    New-Item C:\Task\ -type directory 2>&1 | Out-Null
-    Copy-Item .\Nvidia.ps1 -Destination C:\Task\ 2>&1 | Out-Null
-    $taskname = "Nvidia-Updater"
-    $description = "Update Your Driver!"
-    $action = New-ScheduledTaskAction -Execute "C:\Task\Nvidia.ps1"
-    $trigger = New-ScheduledTaskTrigger -Weekly -WeeksInterval $scheduleTask -DaysOfWeek $scheduleDay -At $scheduleTime
-    Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Description $description 2>&1 | Out-Null
-}
-
-
 # Cleaning up downloaded files
 Write-Host "Deleting downloaded files"
 Remove-Item $nvidiaTempFolder -Recurse -Force
 
 
 # Driver installed, requesting a reboot
-Write-Host -ForegroundColor Green "Driver installed. You may need to reboot to finish installation."
-Write-Host "Would you like to reboot now?"
-$Readhost = Read-Host "(Y/N) Default is no"
-Switch ($ReadHost) {
-    Y { Write-host "Rebooting now..."; Start-Sleep -s 2; Restart-Computer }
-    N { Write-Host "Exiting script in 5 seconds."; Start-Sleep -s 5 }
-    Default { Write-Host "Exiting script in 5 seconds"; Start-Sleep -s 5 }
-}
+#Write-Host -ForegroundColor Green "Driver installed. You may need to reboot to finish installation."
+#Write-Host "Would you like to reboot now?"
+#$Readhost = Read-Host "(Y/N) Default is no"
+#Switch ($ReadHost) {
+#    Y { Write-host "Rebooting now..."; Start-Sleep -s 2; Restart-Computer }
+#    N { Write-Host "Exiting script in 5 seconds."; Start-Sleep -s 5 }
+#    Default { Write-Host "Exiting script in 5 seconds"; Start-Sleep -s 5 }
+#}
 
 
 # End of script
