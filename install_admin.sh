202f0b4c4852b6c56f149166395f6a0b7a1a012f
johanw1232 <73111635+johanw1232@users.noreply.github.com>
2025-02-10 16:08:45 +0100

    stuff

--- install_admin.sh
+++ install_admin.sh
@@ -18,13 +18,13 @@ Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla 
-sudo apt-get update && sudo apt-get install firefox
+sudo apt-get update && sudo apt-get install firefox -y
 
 #unity hub
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 sudo apt update
-sudo apt-get install unityhub
+sudo apt-get install unityhub -y
 
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 # chmod u+x nvim.appimage
